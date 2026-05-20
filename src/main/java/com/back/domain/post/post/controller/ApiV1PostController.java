fef96e8f6b76370e3c122405099e9371fc81e370
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2026-05-20 14:37:02 +0900

    16-2 post전용 rsData 생성

--- src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
@@ -3,6 +3,8 @@ package com.back.domain.post.post.controller;
 import com.back.domain.post.post.dto.PostDto;
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
+import com.back.global.rsData.ForPostRsData;
+import com.back.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
@@ -36,12 +38,16 @@ public class ApiV1PostController {
 
 	@GetMapping("/{id}/delete")
 	@Transactional
-	public PostDto delete(
+	public ForPostRsData delete(
 			@PathVariable int id
 	) {
 		Post post = postService.findById(id).get();
 
 		postService.delete(post);
-		return new PostDto(post);
+		return new ForPostRsData(
+				"200-1",
+				"%d번 글이 삭제되었습니다.".formatted(id),
+				new PostDto(post)
+		);
 	}
 }
---
+++ src/main/java/com/back/global/rsData/ForPostRsData.java
@@ -0,0 +1,10 @@
+package com.back.global.rsData;
+
+import com.back.domain.post.post.dto.PostDto;
+
+public record ForPostRsData(
+		String resultCode,
+		String msg,
+		PostDto data
+){
+}
