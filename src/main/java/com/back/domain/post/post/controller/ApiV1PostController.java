e0f7610815954a1e2388dc8d43f4120d56e801db
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2026-05-21 11:17:26 +0900

    25-1 복잡한 데이터를 담는 ResBody 클래스 추가

--- src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
@@ -57,18 +57,24 @@ public class ApiV1PostController {
 			@NotBlank
 			@Size(min = 2, max = 5000)
 			String content
-	) {
-	}
+	) {}
+
+	public record PostWriteResBody(
+			long totalCount,
+			PostDto post
+	) {}
 
 	@PostMapping
 	@Transactional
-	public RsData<PostDto> write(@Valid @RequestBody PostWriteReqBody form) {
+	public RsData<PostWriteResBody> write(@Valid @RequestBody PostWriteReqBody form) {
 		Post post = postService.write(form.title, form.content);
+
+		PostWriteResBody data = new PostWriteResBody(postService.count(), new PostDto(post));
 
 		return new RsData<>(
 				"200-1",
 				"%d번 글이 생성되었습니다.".formatted(post.getId()),
-				new PostDto(post)
+				data
 		);
 	}
 
