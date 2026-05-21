2a993b4e2b5b153e78614c9b7872f5ef81656826
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2026-05-21 14:18:56 +0900

    33-2 글 생성 테스트 추가

--- src/test/java/com/back/doamin/post/post/controller/ApiV1PostContollerTest.java
+++ src/test/java/com/back/doamin/post/post/controller/ApiV1PostContollerTest.java
@@ -1,4 +1,39 @@
 package com.back.doamin.post.post.controller;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@ActiveProfiles("test")
+@SpringBootTest
+@AutoConfigureMockMvc //MockMvc 를 자동으로 설정 , postman처럼 요청을 보낼 수 있음
+@Transactional
 public class ApiV1PostContollerTest {
+	@Autowired
+	private MockMvc mvc; //mockmvc를 주입받는다. == postman
+
+	//글 작성 테스트
+	@Test
+	@DisplayName("글 쓰기")
+	void t1() throws Exception {
+		ResultActions resultActions = mvc.perform(
+				post("/api/v1/posts")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content("""
+								{
+									"title":"테스트 제목",
+									"content":"테스트 내용"
+								}
+								""")
+		);
+	}
 }
