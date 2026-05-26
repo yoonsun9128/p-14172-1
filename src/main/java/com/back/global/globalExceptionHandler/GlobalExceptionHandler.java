2c39b1251dded768781b5c324038f4c7b470c521
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2026-05-26 14:24:32 +0900

    50-1 제모없이 글 작성 시도

--- src/main/java/com/back/global/globalExceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/back/global/globalExceptionHandler/GlobalExceptionHandler.java
@@ -1,21 +1,49 @@
 package com.back.global.globalExceptionHandler;
 import com.back.global.rsData.RsData;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import lombok.RequiredArgsConstructor;
 
+import java.util.Comparator;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
-@RestControllerAdvice
+@ControllerAdvice
 @RequiredArgsConstructor
 public class GlobalExceptionHandler {
 	@ExceptionHandler(NoSuchElementException.class)
-	@ResponseStatus(NOT_FOUND)
-	public RsData<Void> handle() {
-		return new RsData<>(
-				"404-1",
-				"해당 데이터가 존재하지 않습니다."
+	public ResponseEntity<RsData<Void>> handle(NoSuchElementException ex) {
+		return new ResponseEntity<>(
+				new RsData<>(
+						"404-1",
+						"해당 데이터가 존재하지 않습니다."
+				),
+				NOT_FOUND
+		);
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<RsData<Void>> handle(MethodArgumentNotValidException ex) {
+		String message = ex.getBindingResult()
+				.getAllErrors()
+				.stream()
+				.filter(error -> error instanceof FieldError)
+				.map(error -> (FieldError) error)
+				.map(error -> error.getField() + "-" + error.getCode() + "-" + error.getDefaultMessage())
+				.sorted(Comparator.comparing(String::toString))
+				.collect(Collectors.joining("\n"));
+
+		return new ResponseEntity<>(
+				new RsData<>(
+						"400-1",
+						message
+				),
+				BAD_REQUEST
 		);
 	}
 }
--- src/test/java/com/back/doamin/post/post/controller/ApiV1PostContollerTest.java
+++ src/test/java/com/back/doamin/post/post/controller/ApiV1PostContollerTest.java
@@ -66,6 +66,33 @@ public class ApiV1PostContollerTest {
 	}
 
 	@Test
+	@DisplayName("글 쓰기, without title")
+	void t7() throws Exception {
+		ResultActions resultActions = mvc
+				.perform(
+						post("/api/v1/posts")
+								.contentType(MediaType.APPLICATION_JSON)
+								.content("""
+                                        {
+                                            "title": "",
+                                            "content": "내용"
+                                        }
+                                        """)
+				)
+				.andDo(print());
+
+		resultActions
+				.andExpect(handler().handlerType(ApiV1PostController.class))
+				.andExpect(handler().methodName("write"))
+				.andExpect(status().isBadRequest())
+				.andExpect(jsonPath("$.resultCode").value("400-1"))
+				.andExpect(jsonPath("$.msg").value("""
+                        title-NotBlank-must not be blank
+                        title-Size-size must be between 2 and 100
+                        """.stripIndent().trim()));
+	}
+
+	@Test
 	@DisplayName("글 수정")
 	void t2() throws Exception {
 		int id = 1;
