f9da636d1dbbc3661eccb856b228376b5940359d
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2026-05-27 09:40:56 +0900

    spingDocConfig 추가 및 Info 어노테이셔 추가

---
+++ src/main/java/com/back/global/spingDoc/SpingDocConfig.java
@@ -0,0 +1,16 @@
+package com.back.global.spingDoc;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+		info = @Info(
+				title = "API 서버",
+				version = "beta",
+				description = "API 서버 문서입니다."
+		)
+)
+public class SpingDocConfig {
+}
