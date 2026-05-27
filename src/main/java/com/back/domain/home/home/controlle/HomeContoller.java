71dbd19cedc86f175147ba0f675aa92d6f63295c
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2026-05-27 10:05:29 +0900

    61-1 api 구룹화

--- src/main/java/com/back/domain/home/home/controlle/HomeContoller.java
+++ src/main/java/com/back/domain/home/home/controlle/HomeContoller.java
@@ -1,6 +1,8 @@
 package com.back.domain.home.home.controlle;
 
 import lombok.SneakyThrows;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +23,21 @@ public class HomeContoller {
 				                    <a href="/swagger-ui/index.html">API 문서로 이동</a>
 				                </div>
 				""".formatted(localHost.getHostName(), localHost.getHostAddress());
+	}
+
+	@Bean
+	public GroupedOpenApi groupApiV1() {
+		return GroupedOpenApi.builder()
+				.group("apiV1")
+				.pathsToMatch("/api/v1/**")
+				.build();
+	}
+
+	@Bean
+	public GroupedOpenApi groupController() {
+		return GroupedOpenApi.builder()
+				.group("home")
+				.pathsToExclude("/api/**")
+				.build();
 	}
 }
